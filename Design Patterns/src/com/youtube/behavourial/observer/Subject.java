b3de4a46ecd572a98da459f64fce890b31e7fd3a
= <=>
2015-11-29 19:02:34 +0000

    added the observer pattern

---
+++ Design Patterns/src/com/youtube/behavourial/observer/Subject.java
@@ -0,0 +1,12 @@
+package com.youtube.behavourial.observer;
+
+//This interface handles adding, deleting and updating
+//all observers 
+
+public interface Subject {
+	
+	public void register(Observer o);
+	public void unregister(Observer o);
+	public void notifyObserver();
+	
+}
